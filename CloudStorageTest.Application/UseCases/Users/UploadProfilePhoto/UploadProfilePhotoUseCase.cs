78d255119ad6a7f1d8fb988883a85c4d2b8217ce
denisbatista <denisbatista@F004092>
2024-03-13 19:15:58 -0300

    Add project files.

---
+++ CloudStorageTest.Application/UseCases/Users/UploadProfilePhoto/UploadProfilePhotoUseCase.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace CloudStorageTest.Application.UseCases.Users.UploadProfilePhoto;
+
+public class UploadProfilePhotoUseCase
+{
+    public void Execute(IFormFile file)
+    {
+
+
+    }
+}   
